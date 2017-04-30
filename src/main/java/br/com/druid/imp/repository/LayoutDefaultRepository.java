8e1ebafdb2154e52b1a240a504fc32ccb52a60cf
Daniel Rocha <danielsantosr.rj@gmail.com>
2017-04-30 19:00:17 -0300

    Metodos de filtro

--- src/main/java/br/com/druid/imp/repository/LayoutDefaultRepository.java
+++ src/main/java/br/com/druid/imp/repository/LayoutDefaultRepository.java
@@ -1,11 +1,23 @@
 package br.com.druid.imp.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import br.com.druid.imp.domain.LayoutDefault;
 
 @RestResource(path="layoutDefaultRepository", rel="layoutDefaultRepository")
 public interface LayoutDefaultRepository extends PagingAndSortingRepository<LayoutDefault, Long> {
+	
+	@Query("select ld from LayoutDefault ld "
+			+ " where ( ?1 is null or ld.id = ?1 ) and "
+			+ " ( ?2 is null or ld.name like CONCAT('%',?2,'%') ) ")	
+	Page<LayoutDefault> findByFilters(@Param("id") Long id,
+			@Param("name") String name,
+			Pageable pageable
+			);
 
 }
