638664913b4ed25d997777b4c7d1fbc6dff88212
Daniel Rocha <danielsantosr.rj@gmail.com>
2017-04-30 17:40:43 -0300

    Criacao do Campos do Layout Padrao

--- src/main/java/br/com/druid/imp/domain/LayoutDefault.java
+++ src/main/java/br/com/druid/imp/domain/LayoutDefault.java
@@ -2,12 +2,14 @@ package br.com.druid.imp.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -27,6 +29,9 @@ public class LayoutDefault implements Serializable {
 
 	@Column(name="NOME")
 	private String name;
+	
+	@OneToMany(mappedBy="layoutDefault")
+	private List<FieldLayoutDefault> fieldsLayoutDefault;	
 	
 	@Column(name="FL_STATUS")
 	private String status;
