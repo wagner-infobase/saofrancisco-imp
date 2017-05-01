303480ca119d911876011b2768a170c0777fc919
Thales Castello <thalescastello@gmail.com>
2017-04-30 21:36:15 -0300

    Corrigindo filtro da view do Valida Campo do Layout

--- src/main/java/br/com/druid/imp/repository/VwValidFieldLayoutRepository.java
+++ src/main/java/br/com/druid/imp/repository/VwValidFieldLayoutRepository.java
@@ -12,20 +12,20 @@ import br.com.druid.imp.domain.VwValidFieldLayout;
 @RestResource(path="vwValidFieldLayoutRepository", rel="vwValidFieldLayoutRepository")
 public interface VwValidFieldLayoutRepository extends PagingAndSortingRepository<VwValidFieldLayout, Long> {
 	
-/*	@Query("select vc from VwValidFieldLayout vc "
-			+ " where ( ?1 is null or vc.layout_id = ?1 ) and "
-			+ " ( ?2 is null or vc.campo_layout_id = ?2 ) and "
-			+ " ( ?3 is null or vc.validacao_campo_id = ?3 ) and "
-			+ " ( ?4 is null or vc.nome_layout  like CONCAT('%',?4,'%') ) and"
-			+ " ( ?5 is null or vc.descricao_campo  like CONCAT('%',?5,'%') ) and"
-			+ " ( ?6 is null or vc.numero_campo  like CONCAT('%',?6,'%') )")	
-	Page<VwValidFieldLayout> findByFilters(@Param("layout_id") Long layout_id,
-			@Param("campo_layout_id") Long campo_layout_id,
-			@Param("validacao_campo_id") Long validacao_campo_id,
-			@Param("nome_layout") String nome_layout,
-			@Param("descricao_campo") String descricao_campo,
-			@Param("numero_campo") Integer numero_campo,
+	@Query("select vc from VwValidFieldLayout vc "
+			+ " where ( ?1 is null or vc.layoutId = ?1 ) and "
+			+ " ( ?2 is null or vc.campoLayoutId = ?2 ) and "
+			+ " ( ?3 is null or vc.validacaoCampoId = ?3 ) and "
+			+ " ( ?4 is null or vc.nomeLayout  like CONCAT('%',?4,'%') ) and"
+			+ " ( ?5 is null or vc.descricaoCampo  like CONCAT('%',?5,'%') ) and"
+			+ " ( ?6 is null or vc.numeroCampo  like CONCAT('%',?6,'%') )")	
+	Page<VwValidFieldLayout> findByFilters(@Param("layoutId") Long layoutId,
+			@Param("campoLayoutId") Long campoLayoutId,
+			@Param("validacaoCampoId") Long validacaoCampoId,
+			@Param("nomeLayout") String nomeLayout,
+			@Param("descricaoCampo") String descricaoCampo,
+			@Param("numeroCampo") Integer numeroCampo,
 			Pageable pageable
-			);*/
+			);
 }
 
